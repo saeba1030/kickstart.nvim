70410c86ae7f990291163ec8ef8bda50f146d399
Yifan Liu <saeba1030@gmail.com>
2023-11-06 14:12:49 +0800

    Add keymap for LSP formatting

--- init.lua
+++ init.lua
@@ -435,6 +435,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>fm', vim.lsp.buf.format, 'Formatting')
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
